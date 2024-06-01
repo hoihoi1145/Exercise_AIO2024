6a1f2d3fc13f72dd74aa82eafa22a2e930bcd3bb
hoihoi <oovinc@gmail.com>
2024-06-02 05:16:02 +0700

    Add all files

---
+++ Exercise module 1/Exercise1.py
@@ -0,0 +1,32 @@
+def precision(tp, fp):
+    return tp/(tp+fp)
+
+
+def recall(tp, fn):
+    return tp/(tp+fn)
+
+
+def f1(tp, fp, fn):
+    a, b = precision(tp, fp), recall(tp, fn)
+    return 2*a*b/(a+b)
+
+
+def exercise1(tp, fp, fn):
+    x, d, p = {'tp':tp, 'fp':fp, 'fn':fn}, [], 0
+    for i in x:
+        try:
+            int(x[i])
+        except ValueError:
+            d.append(f'{i} must be int')
+        if x[i] == 0:
+            p = 1
+    if d:
+        for i in d:
+            print(i)
+    elif p:
+        print('tp and fp and fn must be greater than zero')
+    else:
+        print(f'precision is', precision(tp, fp))
+        print(f'recall is', recall(tp, fn))
+        print(f'f1-score is', f1(tp, fp, fn))
+
